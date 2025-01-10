5547206cf361dcdf963be31da94ace33aa14006a
thienminh2k1 <nguyenthienminh09012001@gmail.com>
2025-01-11 00:28:26 +0700

    57

--- src/main/java/vn/hoidanit/jobhunter/controller/AuthController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/AuthController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import vn.hoidanit.jobhunter.domain.dto.LoginDTO;
+import vn.hoidanit.jobhunter.domain.dto.ResLoginDTO;
 import vn.hoidanit.jobhunter.util.SecurityUtil;
 
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,7 +25,7 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginDTO> login(@Valid @RequestBody LoginDTO loginDto){
+    public ResponseEntity<String> login(@Valid @RequestBody LoginDTO loginDto){
 
         //Nạp input gồm username/password vào Security
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword());
@@ -33,7 +34,9 @@ public class AuthController {
         Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
 
         //create a token
-        this.securityUtil.createToken(authentication);
-        return ResponseEntity.ok().body(loginDto);
+        String access_token = this.securityUtil.createToken(authentication);
+        ResLoginDTO res = new ResLoginDTO();
+        res.setAccessToken(access_token);
+        return ResponseEntity.ok().body(access_token);
     }
 }
--- src/main/java/vn/hoidanit/jobhunter/util/SecurityUtil.java
+++ src/main/java/vn/hoidanit/jobhunter/util/SecurityUtil.java
@@ -1,5 +1,7 @@
 package vn.hoidanit.jobhunter.util;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.Base64;
 
 import javax.crypto.SecretKey;
@@ -8,6 +10,10 @@ import javax.crypto.spec.SecretKeySpec;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwsHeader;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,10 +21,29 @@ public class SecurityUtil {
 
     public static final MacAlgorithm JWT_ALGORITHM = MacAlgorithm.HS512;
 
-    @Value("${hoidanit.jwt.token-validity-in-seconds}")
-    private String jwtExpiration;
+    public final JwtEncoder jwtEncoder;
 
-    public void createToken(Authentication authentication) {
+    public SecurityUtil(JwtEncoder jwtEncoder){
+        this.jwtEncoder = jwtEncoder;
+    }
+
+    @Value("${hoidanit.jwt.token-validity-in-seconds}")
+    private long jwtExpiration;
+
+    public String createToken(Authentication authentication) {
+        Instant now = Instant.now();
+        Instant validity = now.plus(this.jwtExpiration, ChronoUnit.SECONDS);
+
+        // @formatter:off
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+            .issuedAt(now)
+            .expiresAt(validity)
+            .subject(authentication.getName())
+            .claim("hoidanit", authentication)
+            .build();
+
+        JwsHeader jwsHeader = JwsHeader.with(JWT_ALGORITHM).build();
+        return this.jwtEncoder.encode(JwtEncoderParameters.from(jwsHeader, claims)).getTokenValue();
 
     }
 }
