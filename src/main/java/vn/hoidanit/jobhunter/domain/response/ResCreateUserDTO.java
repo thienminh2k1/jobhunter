b8f34be30182087a2c1355098355b33be69f10c7
thienminh2k1 <nguyenthienminh09012001@gmail.com>
2025-02-11 06:34:48 +0700

    update minor

--- src/main/java/vn/hoidanit/jobhunter/domain/response/ResCreateUserDTO.java
+++ src/main/java/vn/hoidanit/jobhunter/domain/response/ResCreateUserDTO.java
@@ -1,23 +1,18 @@
-package vn.hoidanit.jobhunter.domain.dto;
+package vn.hoidanit.jobhunter.domain.response;
 
 import java.time.Instant;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import vn.hoidanit.jobhunter.util.constant.GenderEnum;
 
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
-public class ResUserDTO {
+public class ResCreateUserDTO {
     private long id;
-    private String email;
     private String name;
+    private String email;
     private GenderEnum gender;
     private String address;
     private int age;
-    private Instant updatedAt;
     private Instant createdAt;
 }
--- src/main/java/vn/hoidanit/jobhunter/domain/response/ResUserDTO.java
+++ src/main/java/vn/hoidanit/jobhunter/domain/response/ResUserDTO.java
@@ -1,18 +1,23 @@
-package vn.hoidanit.jobhunter.domain.dto;
+package vn.hoidanit.jobhunter.domain.response;
 
 import java.time.Instant;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import vn.hoidanit.jobhunter.util.constant.GenderEnum;
 
 @Getter
 @Setter
-public class ResCreateUserDTO {
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResUserDTO {
     private long id;
-    private String name;
     private String email;
+    private String name;
     private GenderEnum gender;
     private String address;
     private int age;
+    private Instant updatedAt;
     private Instant createdAt;
 }
