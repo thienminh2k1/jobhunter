dfe981ffb8927ac7127567d3686e02b228cb54b4
thienminh2k1 <nguyenthienminh09012001@gmail.com>
2025-03-05 23:20:13 +0700

    #132

---
+++ src/main/java/vn/hoidanit/jobhunter/controller/EmailController.java
@@ -0,0 +1,36 @@
+package vn.hoidanit.jobhunter.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.hoidanit.jobhunter.service.EmailService;
+import vn.hoidanit.jobhunter.service.SubscriberService;
+import vn.hoidanit.jobhunter.util.annotation.ApiMessage;
+
+@RestController
+@RequestMapping("/api/v1")
+public class EmailController {
+
+    private final EmailService emailService;
+    private final SubscriberService subscriberService;
+
+    public EmailController(EmailService emailService,
+            SubscriberService subscriberService) {
+        this.emailService = emailService;
+        this.subscriberService = subscriberService;
+    }
+
+    @GetMapping("/email")
+    @ApiMessage("Send simple email")
+    public String sendSimpleEmail() {
+        // this.emailService.sendSimpleEmail();
+        // this.emailService.sendEmailSync("ads.hoidanit@gmail.com", "test send email",
+        // "<h1> <b> hello </b> </h1>", false,
+        // true);
+        // this.emailService.sendEmailFromTemplateSync("ads.hoidanit@gmail.com", "test
+        // send email", "job");
+        this.subscriberService.sendSubscribersEmailJobs();
+        return "ok";
+    }
+}
